8533e3e5735cb9fde65a6f874538e2aa034dc8d9
Zach <zachhutz0@gmail.com>
2024-10-18 17:03:56 -0400

    replaced placeholder files with my A3

---
+++ UnitTestLoginPage/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BookStoreLIB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BookStoreLIB")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd428798-b299-4c94-aa72-56f4c8df2bdd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestLoginPage/UserData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BookStoreLIB {
+    public class UserData {
+        public int UserId { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public bool LogIn(string loginName, string password) {
+            var dbUser = new DALUserInfo();
+            if (CheckBlankPw(password) || CheckPwStartsWithNonLetter(password) || CheckPwLength(password)) {
+                UserId = -1;
+                return false;
+            }
+            UserId = dbUser.LogIn(loginName, password);
+            if (UserId > 0) {
+                LoginName = loginName;
+                Password = password;
+                Debug.WriteLine("Successful login");
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public bool CheckBlankPw(string password) {
+            if (string.IsNullOrEmpty(password)) {
+                Debug.WriteLine("Password is empty");
+                return true;
+            }
+            return false;
+        }
+        public bool CheckPwStartsWithNonLetter(string password) {
+            if (!char.IsLetter(password[0])) {
+                Debug.WriteLine("Password starts with a non-letter");
+                return true;
+            }
+            return false;
+        }
+        public bool CheckPwLength(string password) {
+            if (password.Length < 6) {
+                Debug.WriteLine("Password is less than 6 characters");
+                return true;
+            }
+            return false;
+        }
+    }
+}
