0b99b5d4533fc397dbcc76e508d04142ec28f292
Deni Rakovic <deni.rakovic23@gmail.com>
2024-10-19 15:38:18 -0400

    init changes

--- BookStore_Agile1/BookStore/Properties/Settings.Designer.cs
+++ BookStore_Agile1/BookStore/Properties/Settings.Designer.cs
@@ -13,10 +13,16 @@ namespace BookStore.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+<<<<<<< Updated upstream:BookStore_Agile1/BookStore/Properties/Settings.Designer.cs
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
 
+=======
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.11.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+>>>>>>> Stashed changes:UnitTestLoginPage/Properties/Settings.Designer.cs
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
 
         public static Settings Default
@@ -26,5 +32,19 @@ namespace BookStore.Properties
                 return defaultInstance;
             }
         }
+<<<<<<< Updated upstream:BookStore_Agile1/BookStore/Properties/Settings.Designer.cs
+=======
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=tfs.cs.uwindsor.ca;Initial Catalog=Agile1DB24;Persist Security Info=T" +
+            "rue;User ID=Agile1DB24;Password=Agile1DB24#;TrustServerCertificate=True")]
+        public string Connection {
+            get {
+                return ((string)(this["Connection"]));
+            }
+        }
+>>>>>>> Stashed changes:UnitTestLoginPage/Properties/Settings.Designer.cs
     }
 }
---
+++ UnitTestLoginPage/BookCatalog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    public class BookCatalog
+    {
+        public DataSet GetBookInfo()
+        {
+            //perform any business logic befor passing to client.
+            // None needed at this time.
+            DALBookCatalog bookCatalog = new DALBookCatalog();
+            return bookCatalog.GetBookInfo();
+        }
+    }
+}
