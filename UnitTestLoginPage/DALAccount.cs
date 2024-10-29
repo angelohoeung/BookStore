544c926ea9121df5907febf657e3d62742df34ae
NgocDoanhTran <tran97@uwindsor.ca>
2024-10-29 16:13:38 -0400

    Build fetching function in DAL for user account

---
+++ UnitTestLoginPage/DALAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreLIB
+{
+    class DALAccount
+    {
+        SqlConnection conn;
+        DataSet dsAccountInfo;
+        public DALAccount()
+        {
+            conn = new SqlConnection(Properties.Settings.Default.Connection);
+        }
+        public DataSet GetAccountInfo()
+        {
+            try
+            {
+                String strSQL = "Select UserID, UserName, Password, Fullname from UserData";
+                SqlCommand cmdGetAccountInfo = new SqlCommand(strSQL, conn);
+                SqlDataAdapter daAccountInfo = new SqlDataAdapter(cmdGetAccountInfo);
+                dsAccountInfo = new DataSet("Books");
+                daAccountInfo.Fill(dsAccountInfo, "Category");            //Get category info
+            }
+            catch (Exception ex) { Debug.WriteLine(ex.Message); }
+            return dsAccountInfo;
+        }
+    }
+}
