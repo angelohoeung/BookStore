429129789c0dd11c1504f7733ab2c75cc686790f
Zach <zachhutz0@gmail.com>
2024-11-01 15:53:33 -0400

    Added XAML for order confirmation and payment, skeletons for code-behind

---
+++ BookStore/PaymentWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookStoreGUI {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class PaymentWindow : Window {
+        public PaymentWindow() {
+            InitializeComponent();
+        }
+
+        public void submitButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        public void cancelButton_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private bool isCardNumberValid(string cardNumber) {
+            return cardNumber.Length == 16 && cardNumber.All(char.IsDigit);
+        }
+
+        private bool isExpirationDateValid(string expirationDate) {
+            if (expirationDate == null || expirationDate == "" || !expirationDate.Contains("/") || expirationDate.Length != 5) {
+                return false;
+            }
+
+            string[] parts = expirationDate.Split('/');
+            if (parts.Length != 2 || !int.TryParse(parts[0], out int month) || !int.TryParse(parts[1], out int year)) {
+                return false;
+            }
+
+            year += 2000;
+            DateTime currentDate = DateTime.Now;
+            DateTime expiration = new DateTime(year, month, 1).AddMonths(1).AddDays(-1);
+
+            return expiration >= currentDate;
+        }
+
+        private bool isSecurityCodeValid(string securityCode) {
+            return securityCode.Length == 3 && securityCode.All(char.IsDigit);
+        }
+
+    }
+}
